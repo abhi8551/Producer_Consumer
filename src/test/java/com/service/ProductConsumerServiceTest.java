da7a1f0cddc1d478cc41771c4619aba7e128ceaf
Abhishek Banthia <abhishek20banthia@gmail.com>
2019-02-18 11:30:31 +0530

    remove commented code

--- src/test/java/com/service/ProductConsumerServiceTest.java
+++ src/test/java/com/service/ProductConsumerServiceTest.java
@@ -4,18 +4,11 @@ import org.junit.Test;
 
 public class ProductConsumerServiceTest {
 
+	@SuppressWarnings("static-access")
 	@Test
 	public void test() {
 		ProducerConsumerService pc = new ProducerConsumerService();
 		pc.main(null);
 		//fail("Not yet implemented");
 	}
-	
-/*	@Test
-	public void test2() {
-        BlockingQueue<Message> queue = new ArrayBlockingQueue<>(10);
-		Producer producer = new Producer(queue);
-		producer.run();
-	}
-*/
 }
